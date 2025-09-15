64889bafa15c34b753774c79602cb2437e2621e4
beezital <63583702+beezital@users.noreply.github.com>
2025-09-15 20:24:21 +0200

    add AppBar (content clipped under the bar)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,68 @@
 import Coordinates from '@/components/Coordinates/Coordinates';
 import AlertList from '@/components/AlertList/AlertList';
 import Map from '@/components/Map/Map';
+import MenuAppBar from '@/components/MenuAppBar/MenuAppBar';
+import { Box, CssBaseline, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+
+const drawerWidth = 240;
 
 
 export default function Home() {
   return (
     <>
-      <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-        <div style={{ display: "flex", flexDirection: "column", gap: "1em", padding: "1em" }}>
-          <h1>Leaflet test</h1>
-          <Coordinates />
-        </div>
-        <Map />
-      </div>
-      <AlertList />
+      <Box sx={{ display: 'flex', height: '100vh' }}>
+        <CssBaseline />
+        <MenuAppBar />
+        <Drawer
+          variant="permanent"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+          }}
+        >
+          <Toolbar />
+          <Box sx={{ overflow: 'auto' }}>
+            <List>
+              {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+            <Divider />
+            <List>
+              {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
+        <Box component="main" sx={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
+          <Toolbar />
+          <div style={{ display: "flex", flexDirection: "column", gap: "1em", padding: "1em" }}>
+            <h1>Leaflet test</h1>
+            <Coordinates />
+          </div>
+          <Map />
+          <AlertList />
+        </Box>
+      </Box>
     </>
   );
 }
