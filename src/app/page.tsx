012b2b1b986a40e5525c3ce13b2491b58a567f44
beezital <63583702+beezital@users.noreply.github.com>
2025-09-17 14:51:20 +0200

    BMAP-2: login with Google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,29 @@ import Map from '@/components/Map/Map';
 import styles from './page.module.css';
 import { AccountCircle } from '@mui/icons-material';
 
+// https://firebase.google.com/docs/web/setup
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged, User } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCX8UamBQeZiFZ6MqFKI-qOGxc7hSSxdRg",
+  authDomain: "beezimap-dev.firebaseapp.com",
+  projectId: "beezimap-dev",
+  storageBucket: "beezimap-dev.firebasestorage.app",
+  messagingSenderId: "968728684510",
+  appId: "1:968728684510:web:795197755788f0e11cdf4f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+
 const drawerWidth = 240;
 
 type Preferences = {
@@ -21,7 +44,7 @@ export default function Home() {
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>();
   const isDesktop = useMediaQuery('(min-width:600px)'); // https://www.browserstack.com/guide/responsive-design-breakpoints
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [auth, setAuth] = useState(true);
+  const [user, setUser] = useState<User | null>(null);
   const [preferences, setPreferences] = useState<Preferences>();
 
 
@@ -47,23 +70,68 @@ export default function Home() {
   };
 
   function logout() {
-    setAuth(false);
     handleClose();
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      setUser(null);
+    }).catch((error) => {
+      // An error happened.
+    });
   }
 
   function login() {
-    setAuth(true);
-    handleClose();
+    // https://firebase.google.com/docs/auth/web/google-signin
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        /*
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+        */
+      }).catch((error) => {
+        console.error("Login error", error);
+        /*
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+        */
+      });
   }
 
   useEffect(() => {
     const prefs = loadPreferences();
     setPreferences(prefs);
     setIsDrawerOpen(prefs.isDrawerOpen);
+
+    // https://firebase.google.com/docs/auth/web/start#set_an_authentication_state_observer_and_get_user_data
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        // const uid = user.uid;
+        // ...
+        console.log("User logged in", user);
+        setUser(user);
+      } else {
+        // User is signed out
+        // ...
+        console.log("User logged out");
+        setUser(null);
+      }
+    });
   }, []);
 
 
-function toggleDrawer() {
+  function toggleDrawer() {
     const newIsDrawerOpen = !isDrawerOpen;
     setIsDrawerOpen(newIsDrawerOpen);
     const newPreferences = { ...preferences, isDrawerOpen: newIsDrawerOpen };
@@ -88,18 +156,9 @@ function toggleDrawer() {
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               beeziMap
             </Typography>
-            {auth ? (
+            {user ? (
               <div>
-                <IconButton
-                  size="large"
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleMenu}
-                  color="inherit"
-                >
-                  <AccountCircle />
-                </IconButton>
+                <Button variant="contained" disableElevation startIcon={<AccountCircle />} onClick={handleMenu}>{user.displayName}</Button>
                 <Menu
                   id="menu-appbar"
                   anchorEl={anchorEl}
